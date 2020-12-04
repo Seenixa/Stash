3dd74ab232041d09425b59912619a599436f82d6
Seenixa <75445689+Seenixa@users.noreply.github.com>
2020-12-04 07:11:30 +0100

    Update main.c

--- main.c
+++ main.c
@@ -1,10 +1,4 @@
-/******************************************************************************
 
-                            Online C Compiler.
-                Code, Compile, Run and Debug C program online.
-Write your code in this editor and press "Run" button to compile and execute it.
-
-*******************************************************************************/
 
 #include <string.h>
 #include <stdio.h>
@@ -14,20 +8,20 @@ char a[100];
 int i, b;
 int v = 'Z' - 'z';
 
-main ()
+void main ()
 {
+char a[100];
+int i, b;
+int v = 'Z' - 'z';
 
-  printf ("Ird be a nagyC-tani kC-vC!nt szC6veget\n");
-  scanf ("%s", a);
-  int A = strlen (a);
-  for (i = 0; i < A; i++)
-    {
-
-      if (a[i] >= 'Z')
-	a[i] = a[i] + v;
-      printf ("\n%c", a[i]);
-
-    }
-  printf ("\n%s", a);
-  return 0;
+printf ("Ird be a nagyitani kivant szoveget\n");
+scanf ("%s", a);
+int A = strlen (a);
+for (i = 0; i < A; i++)
+        {
+        if (a[i] >= 'Z')
+        a[i] = a[i] + v;
+        printf ("\n%c", a[i]);
+        }
+printf ("\n%s", a);
 }
