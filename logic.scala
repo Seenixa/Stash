cad0ce04f37035acc63f8933a8bd47241871d50a
Seenixa <75445689+Seenixa@users.noreply.github.com>
2020-12-08 09:30:26 +0100

    Update logic.scala

--- logic.scala
+++ logic.scala
@@ -6,7 +6,7 @@ object logic
       if (x % 3 != 0 && x % 2 != 0 && x>10)
       {
         println("x 10-nel nagyobb, 2-vel es 3-al nem oszthato szam.")
-        printf("x = %d\n", x)
+        println("x = " + x)
       }
       else println("x 10-nel kisebb, vagy oszthato 2-vel vagy 3-al.")
       
@@ -14,7 +14,7 @@ object logic
       if(y > -5 && y < 5)
       {
         println("y -5 es 5 koze eso szam.")
-        printf("y = %d\n", y)
+        println("y = " + y)
       }
       else println("y -5nel kisebb, vagy 5nel nagyobb.")
    }
