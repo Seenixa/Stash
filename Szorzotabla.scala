89b8ac08ad0035607f2602f0609d322c0c1a5ac8
Seenixa <75445689+Seenixa@users.noreply.github.com>
2020-12-08 12:39:39 +0100

    Update Szorzotabla.scala

--- Szorzotabla.scala
+++ Szorzotabla.scala
@@ -1,14 +1,56 @@
-object Szorzotabla
+object HelloWorld
 {
    def main(args: Array[String]) 
    {
-     for(y <- 1 to 10)
+     for(x <- 0 to 10)
      {
-      if(y>1) println("")
-       for(x <- 1 to 10)
+       println("")
+      
+       for(y <- 0 to 10)
+       {
+          print("" + x*y + " ") 
+       }
+        
+     }
+     
+     
+     println("\n")
+     
+     
+     var x: Int = 0
+     while(x < 11)
+     {
+        println("")
+        var y: Int = 0
+        
+        while(y < 11)
+        {
+           print("" + x*y + " ")
+           y = y + 1
+           
+        }
+      x = x + 1
+     }
+     
+     
+     x = 0
+     println("")
+     
+
+     do
+     {
+       println("")
+       var y: Int = 0
+       
+       do
        {
           print("" + x*y + " ")
+          y = y + 1
        }
+       while(y < 11)
+       
+       x = x + 1
      }
+     while(x < 11)
    }
 }
