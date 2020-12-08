fee42e484385d96e48f516ee45d62e3481496144
Seenixa <75445689+Seenixa@users.noreply.github.com>
2020-12-08 08:50:56 +0100

    Update logic.scala

--- logic.scala
+++ logic.scala
@@ -1,16 +1,16 @@
-object Logic
+object logic
 {
    def main(args: Array[String]) 
    {
-      val x = 12
+      val x: Int = 12
       if (x % 3 != 0 && x % 2 != 0 && x>10)
       {
-      println("x 10-nel nagyobb, 2-vel es 3-al nem oszthato szam.")
-      printf("x = %d\n", x)
+        println("x 10-nel nagyobb, 2-vel es 3-al nem oszthato szam.")
+        printf("x = %d\n", x)
       }
       else println("x 10-nel kisebb, vagy oszthato 2-vel vagy 3-al.")
       
-      val y = 10
+      val y: Int = 10
       if(y > -5 && y < 5)
       {
         println("y -5 es 5 koze eso szam.")
