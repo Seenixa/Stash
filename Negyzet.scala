3cf9d9cb00d76fe4cfe7784a0050d0fda4e5df17
Seenixa <75445689+Seenixa@users.noreply.github.com>
2020-12-08 09:56:27 +0100

    Update Negyzet.scala

--- Negyzet.scala
+++ Negyzet.scala
@@ -3,7 +3,6 @@ object Negyzet
    def main(args: Array[String]) 
    {
      val Addone = (x: Int) => x + 1
-     var x = 0
      for(x <- 0 to 19)
      {
         println("A szam:" + Addone(x) + " A negyzete:" + Addone(x)*Addone(x))
