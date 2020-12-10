2ebe5bece8b6132d4c5c30f9396671ee949182fa
Seenixa <75445689+Seenixa@users.noreply.github.com>
2020-12-10 14:17:25 +0100

    Update Pontozas.scala

--- Pontozas.scala
+++ Pontozas.scala
@@ -2,35 +2,42 @@ object Pontozas
 {
 def main(args: Array[String]) {
     
-    new student("Pistike", 100)
-    new student("Moriczka", 45)
-    new student("Tomika", 90)
-    new student("Nixie", 80)
-    new student("Seenixa", 65)
-    new student("Doma", 49)
-    new student("Davidka", -2)
-    new student("Kutyus", 112)
-
     class student (name: String, points: Int){
-        var osztalyzat = ""
-
-        if(points <= 100 && points > 90)
-            osztalyzat = "jeles"
-        if(points <= 90 && points > 80)
-            osztalyzat = "jo"
-        if(points <= 80 && points > 65)
-            osztalyzat = "kozepes"
-        if(points <= 65 && points > 49)
-            osztalyzat = "elegseges"
-        if(points <= 49 && points >= 0)
-            osztalyzat = "elegtelen"
-
-        if(!(points>100 || points<0))
-            println(name + " a pontszamai alapjan " + osztalyzat + " eredmenyt ert el. (" + points + ")")
-        else if(points>100)
-            println(name + " vagy nagyon okos, vagy rossz a pontszam. (" + points + ")")
-        else if(points<0)
-            println(name + " vagy nagyon... buta, vagy rossz a pontszam.(" + points + ")")
+        def p = points
+        def n = name
     }
+    
+    def grading(points: Int):Int = {
+        if(points <= 100 && points > 90)return 5
+        if(points <= 90 && points > 80) return 4
+        if(points <= 80 && points > 65) return 3
+        if(points <= 65 && points > 49) return 2
+        if(points <= 49 && points >= 0) return 1
+        return 0
+    }
+    
+    var points = new student("Pistike", 78)
+    
+    if(grading(points.p) != 0)
+        println(points.n + " osztalyzata: " + grading(points.p))
+    else println("Hibas pontszam megadas." + points.p + " " + points.n)
+    
+    points = new student("Janika", 100)
+    
+    if(grading(points.p) != 0)
+        println(points.n + "osztalyzata: " + grading(points.p))
+    else println("Hibas pontszam megadas." + points.p + " " + points.n)
+    
+    points = new student("Hulyegyerek", -15)
+    
+    if(grading(points.p) != 0)
+        println(points.n + "osztalyzata: " + grading(points.p))
+    else println("Hibas pontszam megadas." + points.p + " " + points.n)
+    
+    points = new student("cheater", 135)
+    
+    if(grading(points.p) != 0)
+        println(points.n + "osztalyzata: " + grading(points.p))
+    else println("Hibas pontszam megadas." + points.p + " " + points.n)
    }
 }
