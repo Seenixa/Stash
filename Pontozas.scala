11d121d5c50e8e4b06801435ecf243ac6dba968d
Seenixa <75445689+Seenixa@users.noreply.github.com>
2020-12-10 13:27:51 +0100

    Update Pontozas.scala

--- Pontozas.scala
+++ Pontozas.scala
@@ -1,36 +1,33 @@
-object Pontozas
+object HelloWorld
 {
 def main(args: Array[String]) {
     
-    new student("Pistike", 100)
-    new student("Moriczka", 45)
-    new student("Tomika", 90)
-    new student("Nixie", 80)
-    new student("Seenixa", 65)
-    new student("Doma", 49)
-    new student("Davidka", -2)
-    new student("Kutyus", 112)
-
-    class student (name: String, points: Int){
-        var osztalyzat = ""
-
-        if(points <= 100 && points > 90)
-            osztalyzat = "jeles"
-        if(points <= 90 && points > 80)
-            osztalyzat = "jo"
-        if(points <= 80 && points > 65)
-            osztalyzat = "kozepes"
-        if(points <= 65 && points > 49)
-            osztalyzat = "elegseges"
-        if(points <= 49 && points >= 0)
-            osztalyzat = "elegtelen"
-
-        if(!(points>100 || points<0))
-            println(name + " a pontszamai alapjan " + osztalyzat + " eredmenyt ert el. (" + points + ")")
-        else if(points>100)
-            println(name + " vagy nagyon okos, vagy rossz a pontszam. (" + points + ")")
-        else if(points<0)
-            println(name + " vagy nagyon... buta, vagy rossz a pontszam.(" + points + ")")
+    var points = 0
+    
+    def grading(points: Int):Int = {
+        if(points <= 100 && points > 90)return 5
+        if(points <= 90 && points > 80) return 4
+        if(points <= 80 && points > 65) return 3
+        if(points <= 65 && points > 49) return 2
+        if(points <= 49 && points >= 0) return 1
+        return 0
     }
+    
+    points = 67
+    if(grading(points) != 0)
+        println("A tanulo osztalyzata: " + grading(points))
+    else println("Hibas pontszam megadas.")
+    points = -21
+    if(grading(points) != 0)
+        println("A tanulo osztalyzata: " + grading(points))
+    else println("Hibas pontszam megadas.")
+    points = 110
+    if(grading(points) != 0)
+        println("A tanulo osztalyzata: " + grading(points)) 
+    else println("Hibas pontszam megadas.")
+    points = 30
+    if(grading(points) != 0)
+        println("A tanulo osztalyzata: " + grading(points)) 
+    else println("Hibas pontszam megadas.")
    }
 }
