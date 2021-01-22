b6d110de2c11249072a988e49f86a5a2a25d3d13
Seenixa <75445689+Seenixa@users.noreply.github.com>
2021-01-22 12:46:51 +0100

    Update calendar.scala

--- calendar.scala
+++ calendar.scala
@@ -1,33 +1,18 @@
 object calendar{
   def main(args: Array[String]) :Unit ={
     
-    val year = 2018
+    val year = 2021
     
     class month( id:Int, name: String, lengthInDay: Int)
     
     val monthsName = Array[String]( "January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December")
+    val monthsIds = Array[Int]( 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11)
     val days = Array[String]( "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday")
     val daysInt = Array[Int]( 1, 2, 3, 4, 5, 6, 7)
     
     def leapYear(currentYear: Int): Boolean={
       currentYear % 4 == 0
     }
-    
-  /*  def firstDayOfYear( year:Int): String ={
-      var day = ""
-      if( year == 2018)
-        day = days(0)
-      if( year > 2018){
-        val numberOfLeapYears = ( year - 2016) / 4 
-        day = days( ( year % 2018 + numberOfLeapYears) % 7)  
-      }
-      if( year < 2018){
-        val numberOfLeapYears = ( ( year - 2020) / 4).abs
-        day = days( (( ( year - 2) - numberOfLeapYears) % 7).abs) 
-      }
-      day
-    } 
-  */
     
     def firstDayOfYearInt( year:Int): Int ={
       var day = 0
@@ -46,25 +31,11 @@ object calendar{
     
     def firstDayOfMonth( firstDayOfYear: Int, monthsId: Int): Int ={
       var day = firstDayOfYear
-      for( previousMonths <- 0 to monthsId){
-        day = ( monthLengthById(previousMonths, leapYear(year)) - 28 + day) % 7
+      for( previousMonths <- 1 to monthsId){
+        day = ( monthLengthById(previousMonths - 1, leapYear(year)) - 28 + day) % 7
       }
       day
     }
-    
- /*   def monthLengthByName( monthsName: String, leapYear: Boolean) :Int ={
-      var monthLength = 30
-      if( monthsName == "January" || monthsName == "March" || monthsName == "May" || monthsName == "July"
-         || monthsName == "August" || monthsName == "October" || monthsName == "December" )
-        monthLength = 31
-      if( monthsName == "February"){
-        monthLength = 28
-        if( leapYear == true)
-          monthLength = 29
-      }
-      monthLength     
-    }
- */
     
     def monthLengthById( id: Int, leapYear: Boolean) :Int ={
       var monthLength = 30
@@ -87,23 +58,44 @@ object calendar{
       headline
     }
     
-    def displayMonth( monthId: Int, firstDay: Int): String ={
+    def displayMonth: String ={
       var display = ""
-      display = "   1  2  3  4  5  6 \n7  8  9  10 11 12 13 \n"
-      display+= "14 15 16 17 18 19 20 \n21 22 23 24 25 26 27\n28 29 30"
+      val skip = "   "
+      var previousDay = 0
+      for( month <- monthsIds){
+        previousDay = 0
+        for( i <- 0 to 1){
+          display+= displayHeadline( monthsName( month), i) + "\n"
+        }
+        for( i <- 0 to 6){
+          if( (firstDayOfMonth( firstDayOfYearInt( year), month)) > i)
+            display+= skip          
+        }
+        for( i <- 0 until monthLengthById( month, leapYear( year))){
+          if( (firstDayOfMonth( firstDayOfYearInt( year), month) + i) % 7 == 0)
+            display+= "\n"
+          if( i < 9){
+            display+= (previousDay + 1) + "  "
+            previousDay+= 1
+          }
+          if( i >= 9){
+              display+= (previousDay + 1) + " "
+            previousDay+= 1
+          if( i == monthLengthById( month, leapYear( year)) - 1)
+            display+= "\n \n"
+          }
+        }   
+      }
       display
     }
     
-    def calendar(year: Int): Unit ={
-      for(i <- monthsName)
-        for(j <- 0 to 1){
-          println( displayHeadline( i, j))
-          if( j == 1)
-            println( displayMonth( 1, firstDayOfMonth( firstDayOfYearInt( year), 1)) + " \n" + " \n")
-        }
+    def printCalendar(year: Int): Unit ={
+      println( displayMonth + " \n" + " \n")
     }
     
-    calendar(2020)
+
+    printCalendar(2019)
+
 
   }
 }
