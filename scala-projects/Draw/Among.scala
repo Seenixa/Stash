e16ff5b3d01b289f725a7bfb7750d7e606a5181e
Seenixa <75445689+Seenixa@users.noreply.github.com>
2021-05-04 08:47:37 +0200

    Create Among.scala

---
+++ scala-projects/Draw/Among.scala
@@ -0,0 +1,26 @@
+object game extends App{
+  println(s""" @@@@@@@   @@@@@@@  @@@@@@@ @@@  @@@@  @@@  @@@       @@@    @@@@    @@@@@@@  @@@@@@@
+             |@@@@  @@@  @@   @@  @@       @@  @@@@  @@   @@@@     @@@@   @@  @@   @@@@@@@  @@
+             |@@@        @@@@@@   @@@@@@@  @@@  @@  @@@   @@ @@   @@ @@  @@@@@@@@    @@@    @@@@@@@
+             |@@@@  @@@  @@@ @@@  @@        @@ @@@@ @@    @@  @@ @@  @@  @@@  @@@    @@@    @@
+             | @@@@@@@   @@@  @@  @@@@@@@    @@@  @@@     @@   @@@   @@  @@@  @@@    @@@    @@@@@@@
+             |
+             |
+             |
+             |""".stripMargin)
+  
+  
+  println(s"""     RRRR       WWWW        BBBB         OOOO         DDDD         AAAA        XXXX       GGGG
+             |   RRRRRRRR   WWWWWWWW    BBBBBBBB     OOOOOOOO     DDDDDDDD     AAAAAAAA    XXXXXXXX   GGGGGGGG
+             |  RRRR    RR WWWW    WW  BBBB    BB   OOOO    OO   DD    DDDD   AA    AAAA  XX    XXXX GG    GGGG
+             |RRIRRR    RWWIWWW    WWBBIBBB    BB OOIOOO    OO   DD    DDDIDD AA    AAAIAAXX    XXXIXXG    GGGIGG
+             |RRIRRRRRRRRWWIWWWWWWWWWBBIBBBBBBBBB OOIOOOOOOOOO   DDDDDDDDDIDD AAAAAAAAAIAAXXXXXXXXXIXXGGGGGGGGIGG
+             |RRIRRRRRRRRWWIWWWWWWWWWBBIBBBBBBBBB OOIOOOOOOOOO   DDDDDDDDDIDD AAAAAAAAAIAAXXXXXXXXXIXXGGGGGGGGIGG
+             |RRIRRRRRRRRWWIWWWWWWWWWBBIBBBBBBBBB OOIOOOOOOOOO   DDDDDDDDDIDD AAAAAAAAAIAAXXXXXXXXXIXXGGGGGGGGIGG
+             |RRIRRRRRRRRWWIWWWWWWWWWBBIBBBBBBBBB OOIOOOOOOOOO   DDDDDDDDDIDD AAAAAAAAAIAAXXXXXXXXXIXXGGGGGGGGIGG
+             |RRIRRRR RRRWWIWWWW WWWWBBIBBBB BBBB OOIOOOO OOOO   DDDD DDDDIDD AAAA AAAAIAAXXXX XXXXIXXGGG GGGGIGG
+             |   RRRR RRRR  WWWW WWWW   BBBB BBBB    OOOO OOOO   DDDD DDDD    AAAA AAAA   XXXX XXXX  GGGG GGGG
+             |   RRRR RRRR  WWWW WWWW   BBBB BBBB    OOOO OOOO   DDDD DDDD    AAAA AAAA   XXXX XXXX  GGGG GGGG
+             """
+          .stripMargin)
+}
