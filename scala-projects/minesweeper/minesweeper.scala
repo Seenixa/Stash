6d13d86be9adf838dba463e12992e3a7075384a9
Seenixa <75445689+Seenixa@users.noreply.github.com>
2021-05-19 12:55:04 +0200

    Update minesweeper.scala

--- scala-projects/minesweeper/minesweeper.scala
+++ scala-projects/minesweeper/minesweeper.scala
@@ -3,9 +3,9 @@ object minesweeper extends App{
   val r = scala.util.Random
   
   class table(
-    var Xlength :Int = 2,
-    var Ylength :Int = 2,
-    var numberOfMines :Int = 2,
+    var Xlength :Int = 10,
+    var Ylength :Int = 10,
+    var numberOfMines :Int = 90,
   ){    
     var tableSize = Xlength * Ylength
     if( tableSize <= numberOfMines)
@@ -36,7 +36,7 @@ object minesweeper extends App{
       numbersPosition
     }
     
-    def convertMinesPosition(minesPosition: Array[Int]) :Array[mines] ={
+    def convertMinesPosition( minesPosition: Array[Int]) :Array[mines] ={
       var realMinesPosition = new Array[mines](numberOfMines)
       var realMine = new mines()
       for( i <- 0 until minesPosition.length){
@@ -48,7 +48,7 @@ object minesweeper extends App{
       realMinesPosition
     }
     
-    def convertNumbersPosition(numbersPosition: Array[Int]) :Array[numbers] ={
+    def convertNumbersPosition( numbersPosition: Array[Int]) :Array[numbers] ={
       var realNumbersPosition = new Array[numbers](tableSize - numberOfMines)
       var realNumber = new numbers()
       for( i <- 0 until numbersPosition.length){
@@ -59,6 +59,65 @@ object minesweeper extends App{
       }
       realNumbersPosition
     }
+    
+    def displayTable( minesArray: Array[Int], numbersArray: Array[Int]): String ={
+      var stringLine = ""
+      var number = 0
+      var minesPosition = minesArray.toSeq
+      var numbersPosition = numbersArray.toSeq
+      for( columnTile <- 0 until Ylength){
+        stringLine += "\n "
+        for( lineTile <- 0 until Xlength){
+          if( minesPosition.contains(lineTile + (columnTile * Xlength))){
+            stringLine += "X "
+          }
+          if( numbersPosition.contains(lineTile + (columnTile * Xlength))){
+            if( minesPosition.contains(lineTile + (columnTile * Xlength) - 1))                  // Eggyel balra néz            39 41 40 46 / 47 / 48 54 53 55             -3 -1 -2 2 / 3 / 4 8 7 9 
+              if( lineTile != 0)                                                                //                             21 19 20 26 / 27 / 28 34 33 35     
+                number += 1
+            
+            if( minesPosition.contains(lineTile + (columnTile * Xlength) + 1))                  // Eggyel jobbra néz
+              if( lineTile != Xlength - 1)
+                number += 1
+            
+            
+            if( minesPosition.contains(lineTile + (columnTile * Xlength) - Xlength))            // Felfele néz
+              if( columnTile != 0)
+                number += 1
+            
+            
+            if( minesPosition.contains(lineTile + (columnTile * Xlength) + Xlength))            // Lefelé néz
+              if( columnTile != Ylength - 1)
+                number += 1
+            
+            
+            if( minesPosition.contains(lineTile + (columnTile * Xlength) - Xlength - 1))        // Balra felfelé néz
+              if( lineTile != 0 && columnTile != 0)
+                number += 1
+            
+            
+            if( minesPosition.contains(lineTile + (columnTile * Xlength) - Xlength + 1))        // Jobbra felfelé néz
+              if( lineTile != Xlength - 1 && columnTile != 0)
+                number += 1
+            
+            
+            if( minesPosition.contains(lineTile + (columnTile * Xlength) + Xlength - 1))        // Balra lefelé néz
+              if( lineTile != 0 && columnTile != Ylength - 1)
+                number += 1
+            
+            
+            if( minesPosition.contains(lineTile + (columnTile * Xlength) + Xlength + 1))        // Jobbra lefelé néz
+              if( lineTile != Xlength - 1 && columnTile != Ylength - 1)
+                number += 1
+            
+            
+            stringLine += s"$number "
+            number = 0
+          }
+        }
+      }
+     stringLine
+   }
     
   }
   
@@ -73,7 +132,6 @@ object minesweeper extends App{
       else
         s"x: $Xposition     y: $Yposition"
     }
-    
   } 
   
   
@@ -87,27 +145,16 @@ object minesweeper extends App{
         s"x: $Xposition    y: $Yposition"
       else
         s"x: $Xposition     y: $Yposition"
-    }
-    
+    }   
   }
   
   var mine = new table()
   var minepos = mine.getMinesPosition
   var realMines = mine.convertMinesPosition(minepos)
-  for( i <- minepos)
-    println(i)
-  for( i <- realMines)
-    println(i)
-  println(s"""
-              |""".stripMargin)
   var numberpos = mine.getNumbersPosition(minepos)
   var realNumbers = mine.convertNumbersPosition(numberpos)
+  for(i <- minepos)
+  println(i)
+  println(mine.displayTable(minepos, numberpos))
 
-  for(i <- numberpos)
-    println(i)
-  for( i <- realNumbers)
-    println(i)
-  
- 
-  println("it runs")
 }
